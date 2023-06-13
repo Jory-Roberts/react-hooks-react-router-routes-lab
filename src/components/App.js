f89bc0e30845b114de1be2830c271aac9c991519
Jory Roberts <robertsjory13@gmail.com>
2023-06-13 11:34:56 -0500

    Actors Page complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
-import Movies from "./Movies";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './Home';
+import Actors from './Actors';
+import Directors from './Directors';
+import Movies from './Movies';
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{<Actors />}</div>;
 }
 
 export default App;
